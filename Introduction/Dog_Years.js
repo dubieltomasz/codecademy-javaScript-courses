1606313cca59d7b8bf7026e14059b8b744d2b687
Tomasz Dubiel <dubieltomasz420@gmail.com>
2023-09-11 20:49:23 +0200

    Create Dog_Years.js

---
+++ Introduction/Dog_Years.js
@@ -0,0 +1,26 @@
+//variable equal to my age
+let myAge = 20;
+
+//variable named earlyYears that saves value 2
+let earlyYears = 2;
+
+//earlyYears *= 10.5;
+earlyYears = earlyYears * 10.5;
+
+//variable equal to myAge subtracted by 2
+let laterYears = myAge - 2;
+
+//number of dog years accounted for by my later years
+//laterYears *= 4;
+laterYears = laterYears * 4;
+
+//console.log(laterYears);
+
+//earlyYears and laterYears together
+let myAgeInDogYears = earlyYears + laterYears;
+
+//saved my name
+let myName = 'Tomek'.toLowerCase();
+
+//writing the statement
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
